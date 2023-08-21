54c96c5f9d60e096c4445235b73210806b333710
Abdelrahman Elkhouly <40030018+khoulyy@users.noreply.github.com>
2023-08-21 19:59:01 +0300

    Fixed default value of winning core selection

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const p2 = {
 
 const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#winningScoreSelect");
-let winningScore = 3;
+let winningScore = 5;
 let isGameOver = false;
 
 function updateScore(player, opponent) {
